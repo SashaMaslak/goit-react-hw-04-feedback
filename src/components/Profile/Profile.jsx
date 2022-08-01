3a1012086072807dd3a9c7913c18dc618a8d851a
SashaMaslak <owmaslak@gmail.com>
2022-08-01 14:50:31 +0300

    2

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,15 @@
+// import user from 'user.json';
+
+// export const Profile = () => {
+//    return <div></div>
+// }
+
+
+
+{/* <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/> */}
